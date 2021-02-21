4eded01b434bce95a24fdafc9cb763499c86e66f
Rafhael <rafamlf.2011@gmail.com>
2021-02-20 21:40:04 -0300

    small adjust

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 //Pages
 import Home from './routes/Home' 
 
+import Login from './components/Authentication/Login/Login'
+import Register from './components/Authentication/Register/Register'
+import Dashboard from './components/Authentication/Dashboard/Dashboard'
+
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
      <Home />
+     <Router>
+                <div>
+                <Switch>
+                    <Route exact path="/login" render={props => <Login {...props} />}></Route>
+                    <Route exact path="/register" render={props => <Register {...props} />}></Route>
+                    <Route exact path="/dashboard" render={props => <Dashboard {...props} />}></Route>
+                </Switch>
+                </div>
+            </Router>
     </div>
   );
 }
