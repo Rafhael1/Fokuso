8cc8e01650c7575ce1bf59bbdb6d066677ff219e
Rafhael <rafamlf.2011@gmail.com>
2021-02-23 14:37:31 -0300

    register

--- client/src/components/Authentication/Register/Register.js
+++ client/src/components/Authentication/Register/Register.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import './Register.scss'
+
+import { Button } from 'semantic-ui-react'
+
 
 export default function Register() {
+
+    const [inputs, setInputs] = useState({
+        name: "pig",
+        email: "",
+        password: ""
+    })
+
+    const { name, email, password } = inputs;
+
+    const onChange = (e) => {
+        setInputs({...inputs, [e.target.name] : e.target.value})
+    }
+
     return (
         <div>
             <h1>Register</h1>
+            <form>
+                <input type="text" name="name" placeholder="name" className="FormInput"  value={name} onChange={e => onChange(e)} />
+                <input type="email" name="email" placeholder="email" className="FormInput" value={email} onChange={e => onChange(e)} />
+                <input type="password" name="password" placeholder="password" className="FormInput" value={password} onChange={e => onChange(e)} />
+                <Button basic color='green' content='Green' className="FormInput" />
+            </form>
         </div>
     )
 }
