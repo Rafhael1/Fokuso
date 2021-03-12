f810c7b844308d9a4d27978a206d59dafdbb8360
Rafhael <rafamlf.2011@gmail.com>
2021-03-12 14:17:31 -0300

    heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json()); //req body
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")))
+    app.use(express.static(path.join(__dirname, "./client/build")))
 }
 
 //Routes
