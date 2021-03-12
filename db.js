be55442acd66df7d266496a4d51c67c622190303
Rafhael <rafamlf.2011@gmail.com>
2021-03-12 14:23:30 -0300

    heroku

--- db.js
+++ db.js
@@ -2,12 +2,18 @@ const Pool = require("pg").Pool;
 require('dotenv').config();
 
 
-const pool = new Pool({
+const devConfig = {
     user: process.env.db_user,
     password: process.env.db_password,
-    host: "localhost",
-    port: 5432,
+    host: process.env.db_host,
+    port: process.env.db_port,
     database: process.env.db_name
-});
+};
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
 
 module.exports = pool;
