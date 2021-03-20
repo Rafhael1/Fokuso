68490fd87b011cfd8af42224257783c0234d4160
Rafhael <rafamlf.2011@gmail.com>
2021-03-20 01:03:38 -0300

    hungry atm

--- db.js
+++ db.js
@@ -1,19 +1,22 @@
+
 const Pool = require("pg").Pool;
-require('dotenv').config();
+require("dotenv").config();
 
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// };
 
-const devConfig = {
-    user: process.env.db_user,
-    password: process.env.db_password,
-    host: process.env.db_host,
-    port: process.env.db_port,
-    database: process.env.db_name
-};
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = {
-    connectionString: process.env.DATABASE_URL
-}
+const proConfig = process.env.DATABASE_URL; //heroku addons
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
 
 module.exports = pool;
